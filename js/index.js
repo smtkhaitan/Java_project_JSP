30c7369c16932e44c9cd6124ce6d6dbbbb76be3d
Sumit Khaitan <sumitkhaitan@gmail.com>
2016-05-23 01:46:40 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+$("#login-button").click(function(event){
+		 event.preventDefault();
+	 window.location.href='signup1.html';
+	
+});
+
+$("#login-button1").click(function(event){
+		 event.preventDefault();
+	 window.location.href='signup2.html';
+	
+});
+$("#login-button").click(function(event){
+		 event.preventDefault();
+	 window.location.href='join1.html';
+	
+});
